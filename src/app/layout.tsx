c61f784abdc70db2bfbf2f5d34760455349d0f58
Nadir <raremammadov@gmail.com>
2026-01-17 08:28:24 +0400

    Fix icons: Restore link tag method for better reliability

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap" />
+      </head>
 
       <body
         className={`${spaceGrotesk.variable} antialiased`}
